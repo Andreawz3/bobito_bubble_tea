9b61ac8e433e4cb0c9b0a12ce23307885d7008a0
lance-malaga <lmalaga@my.bcit.ca>
2023-11-12 14:07:15 -0800

    stylize navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,21 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import styles from './NavBar.module.css'
 
 // ASSETS
 import cart from '../../images/menu/cart.svg'
 
 export default function NavBar() {
+    const { pathname } = useLocation();
+    const splitLocation = pathname.split("/");
+
     return (
         <nav className={styles.navbar}>
             <Link to={'/'} className={styles.bobito}>bobito</Link>
-            <div>
-                <Link to={'/'}>
+            <div className={styles.nav_links}>
+                <Link to={'/'} className={splitLocation[1] === "" ? styles.active : ""}>
                     HOME
                 </Link>
-                <Link to={'/menu'}>
+                <Link to={'/menu'} className={splitLocation[1] === "menu" ? styles.active : ""}>
                     MENU
                 </Link>
-                <Link to={'/about-us'}>
+                <Link to={'/about-us'} className={splitLocation[1] === "about-us" ? styles.active : ""}>
                     ABOUT US
                 </Link>
                 <img src={cart} alt={'cart'} />
