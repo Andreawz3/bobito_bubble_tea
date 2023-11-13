7fa6ac032f87c97f17c67fc460808ddfa36cb81d
lance-malaga <lmalaga@my.bcit.ca>
2023-11-12 17:09:57 -0800

    convert data to js file

---
+++ src/data/boba.js
@@ -0,0 +1,14 @@
+export const boba = [
+    {
+        name: "Boba Name",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        name: "Boba Name",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        name: "Boba Name",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    }
+]
