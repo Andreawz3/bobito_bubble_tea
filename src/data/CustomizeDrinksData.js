e98c31260a33b0bf6f1b216b2be7b16df783353e
Yanjun Liu <yanjunlbcit@gmail.com>
2023-11-14 01:00:38 -0800

    added customizeDrinks component

---
+++ src/data/CustomizeDrinksData.js
@@ -0,0 +1,93 @@
+export const CustomizeDrinksData = [
+    {
+        "drink" : [
+            {
+                name: "Pearl Milk Tea",
+                price: "$6.75",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            }
+        ]
+    },
+    {
+        "Size": [
+            {
+                name: "Small",
+                price: "-$1",
+            },
+            {
+                name: "Regular",
+                price: null,
+            },
+            {
+                name: "Large",
+                price: "+$1",
+            },
+        ]
+    },
+    {
+        "Sweetness": [
+            {
+                name: "100%",
+            },
+            {
+                name: "70%",
+            },
+            {
+                name: "50%",
+            },
+            {
+                name: "30%",
+            },
+        ]
+    },
+    {
+        "Temperature": [
+            {
+                name: "Regular Ice",
+            },
+            {
+                name: "Extra Ice",
+            },
+            {
+                name: "Less Ice",
+            },
+            {
+                name: "No Ice",
+            },
+            {
+                name: "Warm",
+            },
+            {
+                name: "Hot",
+            },
+        ]
+    },
+    {
+        "Topping": [
+            {
+                name: "Pearls",
+                price: "+$1",
+            },
+            {
+                name: "Sago",
+                price: "+$1",
+            },
+            {
+                name: "Grass Jelly",
+                price: "+$1",
+            },
+            {
+                name: "Coconut Jelly",
+                price: "+$1",
+            },
+            {
+                name: "Pudding",
+                price: "+$1",
+            },
+            {
+                name: "Fresh Taro",
+                price: "+$1",
+            },
+        ]
+    },
+]
