30bfdb9c17df6eb270711c0bb1d4c9468c01afeb
Andrea Wu <andrea.wzg@gmail.com>
2023-11-15 16:50:27 -0800

    add navbar to 'about' page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,37 @@
 import styles from '../styles/AboutUs.module.css'
+import { boba } from '../data/boba'
+
+// COMPONENTS
+import NavBar from '../components/NavBar'
+
+// Images
+import bubbleTea from '../images/assets/milk_tea_with_pearl.png'
 
 export default function AboutUs() {
     return (
         <>
-            <h1>About Us</h1>
+            <NavBar />
+            <div className={styles.aboutContainer}>
+                <div>
+                    <div>
+                        <h1>
+                            Stirring Up Smiles, One Sip at a Time
+                        </h1>
+                        <p>
+                            Welcome to our bubble tea haven! At “Bobito”, we are passionate about 
+                            crafting the most delightful and unique bubble tea experiences for our 
+                            customers. Our journey began with a simple desire to share the joy of 
+                            this beloved Taiwanese tradition, and it has since blossomed into a 
+                            commitment to delivering the highest quality and most innovative bubble 
+                            tea creations
+                        </p>
+                    </div>
+                    <img 
+                        src={bubbleTea} 
+                        alt="boba"
+                    />
+                </div>
+            </div>
         </>
     )
 }
