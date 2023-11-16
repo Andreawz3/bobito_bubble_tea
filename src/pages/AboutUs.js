cf5ce70144be59df5a8695e2ed53e316ff081ea0
Andrea Wu <andrea.wzg@gmail.com>
2023-11-15 22:25:23 -0800

    add team members images and information in 'about' page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -6,15 +6,15 @@ import NavBar from '../components/NavBar'
 
 // Images
 import bubbleTea from '../images/assets/milk_tea_with_pearl.png'
-
 export default function AboutUs() {
+
     return (
         <>
             <NavBar />
             <div className={styles.aboutContainer}>
                 <div className={styles.infoContainer}>
                     <div>
-                        <h1 style={{marginBottom: '2rem'}}>Stirring Up Smiles, One Sip at a Time</h1>
+                        <h1 style={{marginBottom: '1.3em'}}>Stirring Up Smiles, One Sip at a Time</h1>
                         <p className={styles.aboutParagraph}>
                             Welcome to our bubble tea haven! At “Bobito”, we are passionate about 
                             crafting the most delightful and unique bubble tea experiences for our 
@@ -23,7 +23,7 @@ export default function AboutUs() {
                             commitment to delivering the highest quality and most innovative bubble 
                             tea creations
                         </p>
-                        <h1 style={{marginBottom: '2rem', marginTop: '2rem'}}>Our Vision</h1>
+                        <h1 style={{marginBottom: '1.3em', marginTop: '1.3em'}}>Our Vision</h1>
                         <p className={styles.aboutParagraph}>
                             To be the premier destination for bubble tea enthusiasts, offering a world 
                             of innovative flavors and a hub for the bubble tea community to connect, 
@@ -38,11 +38,56 @@ export default function AboutUs() {
                             a personalized and delightful bubble tea experience to every doorstep
                         </p>
                     </div>
-                    <img 
-                        className={styles.infoImage}
-                        src={bubbleTea} 
-                        alt="boba"
-                    />
+                    <img className={styles.infoImage} src={bubbleTea} alt="boba" />
+                </div>
+                <h1 style={{marginTop: '4em'}}>Meet Our Team</h1>
+                <div>
+                    <div className={styles.imageContainer}>
+                        <div className={`${styles.memberImg} ${styles.member}`}>
+                            <div className={styles.memberDetail}>
+                                <div className={styles.designIcon}>
+                                    <i class='fas fa-palette'></i>
+                                </div>
+                                <div>
+                                    <h4 className={styles.memberName}>Y. TAEYEON</h4>
+                                    <p className={styles.memberTitle}>Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.memberImg} ${styles.member1}`}>
+                            <div className={styles.memberDetail}>
+                                <div className={styles.designIcon}>
+                                    <i class='fas fa-palette'></i>
+                                </div>
+                                <div>
+                                    <h4 className={styles.memberName}>D. MOMO</h4>
+                                    <p className={styles.memberTitle}>Designer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.memberImg} ${styles.member2}`}>
+                            <div className={styles.memberDetail}>
+                                <div className={styles.designIcon}>
+                                    <i class='fas fa-laptop-code'></i>
+                                </div>
+                                <div>
+                                    <h4 className={styles.memberName}>L. YEON</h4>
+                                    <p className={styles.memberTitle}>Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={`${styles.memberImg} ${styles.member3}`}>
+                            <div className={styles.memberDetail}>
+                                <div className={styles.designIcon}>
+                                    <i class='fas fa-laptop-code'></i>
+                                </div>
+                                <div>
+                                    <h4 className={styles.memberName}>A. YIFEI</h4>
+                                    <p className={styles.memberTitle}>Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
